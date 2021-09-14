a6cd58fc366570e309f3443a08dcc97cfa95dc3e
kushidou <tangzhen1219@gmail.com>
2021-09-14 14:16:26 +0800

    Initial commit

---
+++ usr/bin/touchmap-udev.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -- coding: UTF-8 --
+# -- Author: Blues --
+
+import pyudev
+import os
+
+context = pyudev.Context()
+monitor = pyudev.Monitor.from_netlink(context)
+monitor.filter_by(subsystem='hid')
+for device in iter(monitor.poll, None):
+    if device.driver != "hid-multitouch":
+        continue
+    if device.action == "add" or device.action == "change":
+        os.system("/var/geit-touchscreen/remap-screen")
