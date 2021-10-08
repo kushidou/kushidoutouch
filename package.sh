06c7886d0d394c9e78dd311136dac8f206925754
kushidou <tangzhen1219@gmail.com>
2021-10-08 15:45:48 +0800

    1.1.4
    *文件结构更改
    *增加语言支持
    *调整依赖需求
    *增加安装卸载日志
    *修复一些小bug

--- package.sh
+++ package.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
-# 2021-09-15 15:40 +8
+# 2021-09-15 15:40
+# 2021-09-30 11:00
+# 2021-10-08 11:10
 # copyright: kushidou
 
-echo -e "input your PASSWD(plaintext):\c"
-read ROOTPASSWD
+POSTINST=./kushidoutouch/DEBIAN/postinst
 
-echo $ROOTPASSWD | sudo -S chmod 755 ./kushidoutouch/DEBIAN
-echo $ROOTPASSWD | sudo -S chmod 755 ./kushidoutouch/DEBIAN/*
+echo "Checking [control] mode..."
+if [ $(ls -l ./kushidoutouch/DEBIAN/control | cut -d ' ' -f1) != "-rwxr-xr-x" ];then
+    echo "[control] != 0755!"
+    echo -e "Input your PASSWD(plaintext):\c"
+    read ROOTPASSWD
+    echo $ROOTPASSWD | sudo -S chmod 755 ./kushidoutouch/DEBIAN
+    echo $ROOTPASSWD | sudo -S chmod 755 ./kushidoutouch/DEBIAN/*
+fi
+
 # get version
 vers=$(cat ./kushidoutouch/DEBIAN/control | grep 'Version:' | cut -d ' ' -f 2)
 arch=$(cat ./kushidoutouch/DEBIAN/control | grep 'Architecture:' | cut -d ' ' -f 2)
+
+sed -i "3c BUILDDATE=\"$(date)\"" $POSTINST
+sed -i "4c KTVERSION=\"$vers\"" $POSTINST
+
+gzip ./kushidoutouch/usr/share/kushidoutouch/changelog
+
 echo "ver=$vers, arch=$arch"
+echo "Packing..."
+dpkg -b ./kushidoutouch ./kushidouTouch-${vers}_${arch}.deb
 
-# check mod
-echo $ROOTPASSWD | sudo -S chmod +x ./kushidoutouch/var/kushidou-touchscreen/*-screen
-echo $ROOTPASSWD | sudo -S chmod +x -R ./kushidoutouch/var/kushidou-touchscreen/desktops/*.desktop
-
-dpkg -b ./kushidoutouch ./kushidouTouch-${vers}_${arch}.deb > /dev/null
-echo $ROOTPASSWD | sudo -S sudo chmod 755 *.deb
-echo "Please check kushidouTouch-${vers}_${arch}.deb"
+if [[ 0 -ne $? ]];then
+    echo -e "\e[47;31mError\e[0m"
+else
+    echo "Please check kushidouTouch-${vers}_${arch}.deb"
+fi
